c1c810e84ad290bc667a242f8529d530507c4369
Dudko Mikhail <dudkomikhail01@gmail.com>
2022-11-11 15:01:43 +0300

    add builder

--- src/main/java/by/dudko/carsales/model/entity/PhoneNumber.java
+++ src/main/java/by/dudko/carsales/model/entity/PhoneNumber.java
@@ -1,10 +1,12 @@
 package by.dudko.carsales.model.entity;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class PhoneNumber implements BaseEntity<Long> {
