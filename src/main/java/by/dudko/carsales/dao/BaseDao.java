8d2ebe7f10742b12130e66438b2e1ffd47888867
Dudko Mikhail <dudkomikhail01@gmail.com>
2022-11-23 22:22:44 +0300

    update dao layer with hibernate

--- src/main/java/by/dudko/carsales/dao/BaseDao.java
+++ src/main/java/by/dudko/carsales/dao/BaseDao.java
@@ -1,15 +1,13 @@
 package by.dudko.carsales.dao;
 
-import by.dudko.carsales.exception.DaoException;
 import by.dudko.carsales.model.entity.BaseEntity;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface BaseDao<T, E extends BaseEntity<T>> {
-    List<E> findAll() throws DaoException;
-    Optional<E> findById(T id) throws DaoException;
-    void insert(E entity) throws DaoException;
-    boolean deleteById(T id) throws DaoException;
-
+    List<E> findAll();
+    Optional<E> findById(T id);
+    void insert(E entity);
+    boolean deleteById(T id);
 }
