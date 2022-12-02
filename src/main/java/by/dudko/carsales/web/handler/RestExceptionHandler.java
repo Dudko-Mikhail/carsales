49e276e33c6dcaa2d84f6e80554903d7b59f3d5c
Dudko Mikhail <dudkomikhail01@gmail.com>
2022-12-02 23:25:12 +0300

    added error handling

--- src/main/java/by/dudko/carsales/web/handler/RestExceptionHandler.java
+++ src/main/java/by/dudko/carsales/web/handler/RestExceptionHandler.java
@@ -1,17 +1,33 @@
 package by.dudko.carsales.web.handler;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.TypeMismatchException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+@RequiredArgsConstructor
 @RestControllerAdvice
 public class RestExceptionHandler extends ResponseEntityExceptionHandler {
+    private final ErrorResponseFactory responseFactory;
+
     @Override
-    protected ResponseEntity<Object> handleBindException(BindException ex, HttpHeaders headers, HttpStatus status, WebRequest request) { // todo implement
-        return super.handleBindException(ex, headers, status, request);
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        return new ResponseEntity<>(responseFactory.of(ex), headers, status);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMissingServletRequestParameter(MissingServletRequestParameterException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        return new ResponseEntity<>(responseFactory.of(ex), headers, status);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleTypeMismatch(TypeMismatchException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        return new ResponseEntity<>(responseFactory.of(ex), headers, status);
     }
 }
