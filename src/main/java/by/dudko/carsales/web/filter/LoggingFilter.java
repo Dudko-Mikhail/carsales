bd7547a8e9461e085c8ae9df52cf072bee729bce
Dudko Mikhail <dudkomikhail01@gmail.com>
2022-12-02 23:29:36 +0300

    fix

--- src/main/java/by/dudko/carsales/web/filter/LoggingFilter.java
+++ src/main/java/by/dudko/carsales/web/filter/LoggingFilter.java
@@ -1,13 +1,13 @@
 package by.dudko.carsales.web.filter;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Component;
-
 import java.io.IOException;
 
 @Slf4j
