bee8b966756b66f35effa371a3e5976bafdc29d8
Dudko Mikhail <dudkomikhail01@gmail.com>
2022-12-02 13:39:59 +0300

    add logging filter

---
+++ src/main/java/by/dudko/carsales/web/filter/LoggingFilter.java
@@ -0,0 +1,23 @@
+package by.dudko.carsales.web.filter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Slf4j
+@Component
+public class LoggingFilter extends HttpFilter {
+    @Override
+    protected void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
+        log.info("Request method: {}, URI: {}, time: {}", request.getMethod(), request.getRequestURI(),
+                System.currentTimeMillis());
+        chain.doFilter(request, response);
+        log.info("Response status: {}, time: {}", response.getStatus(), System.currentTimeMillis());
+    }
+}
