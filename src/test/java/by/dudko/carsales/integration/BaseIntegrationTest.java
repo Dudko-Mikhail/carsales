035367553f90ec1132d8acf5b98aedd654b95bee
Dudko Mikhail <dudkomikhail01@gmail.com>
2023-01-20 20:55:20 +0300

    fix

--- src/test/java/by/dudko/carsales/integration/BaseIntegrationTest.java
+++ src/test/java/by/dudko/carsales/integration/BaseIntegrationTest.java
@@ -1,6 +1,5 @@
 package by.dudko.carsales.integration;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.context.jdbc.Sql;
@@ -9,18 +8,19 @@ import org.testcontainers.containers.PostgreSQLContainer;
 @IntegrationTest
 @Sql("classpath:/data.sql")
 public abstract class BaseIntegrationTest {
-    private static final PostgreSQLContainer<?> container = new PostgreSQLContainer<>("postgres:15.0");
+    private static final PostgreSQLContainer<?> container;
 
-    @BeforeAll
-    static void configureContainer() {
+    static {
+        container = new PostgreSQLContainer<>("postgres:15.0");
         container.withUsername("test")
-                .withPassword("test")
-                .start();
+                .withPassword("test");
+        container.start();
     }
 
     @DynamicPropertySource
     static void postgresProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.username", container::getUsername);
+        registry.add("spring.datasource.password", container::getPassword);
         registry.add("spring.datasource.url", container::getJdbcUrl);
-
     }
 }
